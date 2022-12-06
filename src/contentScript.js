446de799b48cf9b03b8ad333feba40192c2c38e7
Vivek Kalyan <hello@vivekkalyan.com>
2022-12-06 22:53:15 +0800

    Format codeblocks with ``` and newlines

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,7 +1,33 @@
 function updateData() {
   var divs = Array.from(document.getElementsByTagName("div"))
   var conversationItems = divs.filter(div => Array.from(div.classList).some(cls => cls.includes("text-base")))
-  var conversationTexts = conversationItems.map(item => item.innerText)
+  let conversationTexts = [];
+  for (const conversationItem of conversationItems) {
+    console.log(conversationItem)
+    const para_code_blocks = conversationItem.querySelectorAll(["p", "pre"]);
+    if (para_code_blocks.length == 0) {
+      // questions
+      conversationTexts.push(conversationItem.innerText);
+    } else {
+      //answers
+      conversationParas = [];
+      for (const block of para_code_blocks) {
+        console.log(block.tagName)
+        if (block.tagName == "P") {
+          let text = block.innerHTML;
+          text = text.replace(/<\/?code>/g, "`");
+          conversationParas.push(text);
+        } else if (block.tagName == "PRE") {
+          let text = block.innerText;
+          text = text.replace(/^Copy code\n/, "```\n");
+          text = text.replace(/\n$/, "\n```");
+          conversationParas.push(text);
+        }
+      }
+      console.log(conversationParas);
+      conversationTexts.push(conversationParas.join("\n\n"))
+    }
+  }
   console.log(conversationTexts);
   chrome.storage.local.get("treeData2", function(items) {
     conversationTree = JSON.parse(items["treeData2"] || JSON.stringify([]));
